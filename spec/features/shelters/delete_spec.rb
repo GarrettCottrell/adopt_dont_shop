3229bf2e9749cc01a11aaadc66b85f8018467510
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-10-10 17:45:41 -0600

    finished all shelter tests

---
+++ spec/features/shelters/delete_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "shelters update page", type: :feature do
+  it "When I visit a shelter show page, there is a link to delete the shelter, 'delete' and when I click this link, the shelter is deleted and I am redirected to the shelter index page" do
+    shelter_1 = Shelters.create(name:        "Sad Shelter",
+                                address:     "12345 Cherry Court",
+                                city:        "San Jose",
+                                state:       "California",
+                                zip:         95032)
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_button("delete")
+    click_on "delete"
+    expect(current_path).to eq("/shelters")
+    
+    expect(current_path).to eq("/shelters")
+    expect(page).to_not have_content("Sad Shelter")
+  end
+end
