cc0a7ceba277fad000e91b21126532b816f93f79
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-10-11 01:26:27 -0600

    created feature for user story 10 -shelter pet creation

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -3,4 +3,22 @@ class ShelterPetsController < ApplicationController
     @shelters = Shelter.find(params[:id])
     @pets = @shelters.pets
   end
+
+  def new
+    @shelters = Shelter.find(params[:id])
+  end
+
+  def create
+    pets = Pet.new({
+      image: params[:image],
+      name: params[:name],
+      description: params[:description],
+      age: params[:age],
+      sex: params[:sex],
+      adoption_status: params[:adoption_status],
+      shelter_id: params[:shelter_id]
+                 })
+    pets.save
+    redirect_to "/shelters/#{pets.shelter_id}/pets"
+  end
 end
